93f24be6b91cee35e4896ef450879a57e5910d06
Karl Susi <karlbeast2002@gmail.com>
2016-11-10 15:01:24 -0800

    MEMES
    LOL

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,20 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
+function SP1() {
+var canvas = document.getElementById("game-board")
+var mark = document.createElementNS(namespace, "circle")
+mark.setAttribute("cx", "100")
+mark.setAttribute("cy", "100")
+mark.setAttribute("r", "50")
+mark.setAttribute("fill", "blue")
+canvas.appendChild(mark)
+
+var alert;
+    if (mark  10) {
+
+    } else if (fill < ) 
+    } else {
+}
+ var memes =
